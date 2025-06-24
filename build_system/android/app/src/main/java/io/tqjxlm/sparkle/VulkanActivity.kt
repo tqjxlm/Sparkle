772cd50fb1489e8874b381d4b1c63ce0a0fedff5
tqjxlm <tqjxlm@gmail.com>
2025-06-24 16:54:29 +0100

    [Android] pop dependency versions. fix warnings in VulkanActivity.kt

--- build_system/android/app/src/main/java/io/tqjxlm/sparkle/VulkanActivity.kt
+++ build_system/android/app/src/main/java/io/tqjxlm/sparkle/VulkanActivity.kt
@@ -1,10 +1,6 @@
 package io.tqjxlm.sparkle
 
-import android.annotation.SuppressLint
-import android.os.Build.VERSION
-import android.os.Build.VERSION_CODES
 import android.os.Bundle
-import android.view.KeyEvent
 import android.view.View
 import android.view.WindowManager.LayoutParams
 import androidx.core.view.WindowInsetsCompat
@@ -18,11 +14,8 @@ class VulkanActivity : GameActivity() {
     }
 
     private fun hideSystemUI() {
-        @SuppressLint("ObsoleteSdkInt")
-        if (VERSION.SDK_INT >= VERSION_CODES.P) {
-            window.attributes.layoutInDisplayCutoutMode =
-                LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
-        }
+        window.attributes.layoutInDisplayCutoutMode =
+            LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
         val decorView: View = window.decorView
         val controller = WindowInsetsControllerCompat(
             window,
@@ -32,15 +25,6 @@ class VulkanActivity : GameActivity() {
         controller.hide(WindowInsetsCompat.Type.displayCutout())
         controller.systemBarsBehavior =
             WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-    }
-
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        var processed = super.onKeyDown(keyCode, event);
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            onBackPressed()
-            processed = true
-        }
-        return processed
     }
 
     companion object {
