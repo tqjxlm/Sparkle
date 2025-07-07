21aa018951471e200b7a97fc43338ce4631749df
tqjxlm <tqjxlm@gmail.com>
2025-07-07 17:34:06 +0100

    [macOS] proper archiving for distribution of the macOS build.

--- build_system/macos/build.py
+++ build_system/macos/build.py
@@ -22,6 +22,70 @@ def get_app_path(args):
     return app_path
 
 
+def try_archiving(app_path, archive_path):
+    # your apple id. e.g. abc@gmail.com
+    username = os.environ.get("APPLE_NOTARIZATION_USERNAME")
+    # your notarization password. see https://support.apple.com/en-us/102654
+    password = os.environ.get("APPLE_NOTARIZATION_PASSWORD")
+    # your developer team id. see https://developer.apple.com/help/account/manage-your-team/locate-your-team-id/
+    team_id = os.environ.get("APPLE_DEVELOPER_TEAM_ID")
+    # your signing identity. Go to https://developer.apple.com/account/resources/certificates/ and add one for "Developer ID Application"
+    signing_identity = os.environ.get("APPLE_SIGNING_IDENTITY")
+
+    if not username or not password or not team_id or not signing_identity:
+        print("Archiving is not fully configured. discard.")
+        return False
+
+    # Sign the app bundle first
+    sign_cmd = ["codesign", "--force", "--options",
+                "runtime", "--sign", signing_identity, app_path]
+    completion = subprocess.run(sign_cmd)
+
+    if completion.returncode != 0:
+        print("Failed to sign the app bundle. discard.")
+        return False
+
+    # Create ZIP for submission
+    compress_cmd = ["ditto", "-c", "-k",
+                    "--keepParent", app_path, archive_path]
+    completion = subprocess.run(compress_cmd)
+
+    if completion.returncode != 0:
+        print("Failed to create archive for notarization. discard.")
+        return False
+
+    # Submit to Apple for a notarization ticket (which will be saved on apple server)
+    notarize_cmd = [
+        "xcrun", "notarytool", "submit", archive_path,
+        "--apple-id", username,
+        "--password", password,
+        "--team-id", team_id,
+        "--wait"
+    ]
+    completion = subprocess.run(notarize_cmd)
+
+    if completion.returncode != 0:
+        print("Failed to notarize with notarytool. discard.")
+        return False
+
+    # Staple ticket to the original .app (request ticket from the apple server)
+    staple_cmd = ["xcrun", "stapler", "staple", app_path]
+    completion = subprocess.run(staple_cmd)
+
+    if completion.returncode != 0:
+        print("Failed to staple. discard.")
+        return False
+
+    # Compress again to create the final distribution ZIP
+    completion = subprocess.run(compress_cmd)
+
+    if completion.returncode != 0:
+        print("Failed to create final archive after notarization. discard.")
+        return False
+
+    return True
+
+
 class MacosBuilder(FrameworkBuilder):
     """macOS framework builder implementation."""
 
@@ -96,12 +160,18 @@ class MacosBuilder(FrameworkBuilder):
 
     def archive(self, args):
         """Archive the built project."""
+
         output_dir = get_output_dir()
         app_path = get_app_path(args)
         archive_path = os.path.join(output_dir, "product.zip")
 
-        # TODO: archive and sign
-        compress_zip(app_path, archive_path)
+        # try archiving with apple toolchain
+        archived = try_archiving(app_path, archive_path)
+
+        # not able to archive, leave it as is (may be processed by external workflows, e.g. github actions)
+        if not archived:
+            print("Failed to archive the app, just create a plain zip archive")
+            compress_zip(app_path, archive_path)
 
         return archive_path
 
